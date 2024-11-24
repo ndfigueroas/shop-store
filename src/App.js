f80a06bee859b4425fb6fe3a93c73ffad7beb0fe
ndfigueroas <ndfigueroas@gmail.com>
2024-11-24 13:10:36 -0500

    crear nav en react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Inicio</Link>
+        <Link to="/sobre-mi">Sobre mí</Link>
+        <Link to="/contacto">Contacto</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobre-mi" element={<SobreMi />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </Router>
   );
+}
+
+function Inicio() {
+  return <h1>Bienvenido a la página de inicio</h1>;
+}
+
+function SobreMi() {
+  return <h1>Esta es la página "Sobre mí"</h1>;
+}
+
+function Contacto() {
+  return <h1>Contáctame en: contacto@example.com</h1>;
 }
 
 export default App;
